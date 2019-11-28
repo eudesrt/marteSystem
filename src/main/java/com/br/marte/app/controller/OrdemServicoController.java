093d1430db7e5a593e20cac26dc8b4354850bfdc
Rafael <Rafael@DESKTOP-64F1A6B>
2019-11-28 19:35:13 -0300

    Add a msg : A  OS $ Já existe no sistema !!!!

--- src/main/java/com/br/marte/app/controller/OrdemServicoController.java
+++ src/main/java/com/br/marte/app/controller/OrdemServicoController.java
@@ -113,7 +113,7 @@ public class OrdemServicoController {
 				message =  "OS salvo com sucesso!";
 
 			}else {
-				message = "Já existe uma OS " + ordemServicoModel.getOs() + " Cadastrada";
+				message = "A  OS " + ordemServicoModel.getOs() + " Já existe no sistema !!!!";
 				valida = false;
 			}
 
