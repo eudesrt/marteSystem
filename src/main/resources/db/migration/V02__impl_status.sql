9ec7ac7647e6ced014fc9d7368fd968593ebce09
Rafael <Rafael@DESKTOP-64F1A6B>
2020-10-07 20:00:40 -0300

    Correção dos campos

--- src/main/resources/db/migration/V02__impl_status.sql
+++ src/main/resources/db/migration/V02__impl_status.sql
@@ -10,3 +10,4 @@ insert into tb_status (evento_id,status) values (1200,'HOMOLOGANDO');
 insert into tb_status (evento_id,status) values (1300,'PENDENTE INFORMACAO');
 insert into tb_status (evento_id,status) values (1400,'GERENCIA');
 insert into tb_status (evento_id,status) values (9999,'FECHADO');
+insert into tb_status (evento_id,status) values (9998,'CANCELADA');
